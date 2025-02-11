0e7d1e7c478f390001046ab2250addeabc02e69b
L17 <a.alabdullatif.17@gmail.com>
2025-02-10 20:37:34 -0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+/***************************************************************
+ * service-worker.js
+ *
+ * A simple service worker to cache essential files for offline use.
+ ***************************************************************/
+
+const CACHE_NAME = "head-soccer-cache-v1";
+const ASSETS_TO_CACHE = [
+  "/",                // so index.html can be served
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install event - cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate event - clean up old caches if needed
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event - serve from cache if available
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
